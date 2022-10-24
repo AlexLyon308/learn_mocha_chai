ae0406acc2d4263707b8aaf80726c5e4bf4bf4ec
DESKTOP-T6FL8DH\Thanh <thanh.dao@eastgate-software.com>
2022-10-24 18:07:12 +0700

    add TimeOut for js_test

--- test/js_test.js
+++ test/js_test.js
@@ -2,12 +2,15 @@ import person from "./js_collection.js";
 import { assert, expect } from 'chai';
 
 describe('JS learn Chai Mocha ', function() {
+  
+  this.timeout(1000); 
 
   before('Execute Before All Tests', function(){
     console.log('All tests are being executed');
 });
 
 beforeEach('Execute Before Each Test', function(){
+    
     console.log('The test has started');
 });
 
@@ -17,18 +20,20 @@ after('Execute After All Tests', function(){
 });
 
 afterEach('Execute After Each Test', function(){
+    
     console.log('The test has finished and showed result');
 });
 
-  it('test 1', function () {
+  it('test 1', function (done) {
       
       expect(person.fullName()).to.equal("Avita Lisbaron");
-      
+      setTimeout(done,500);
+
   });
 
-  it('test 2', function () {
-      
+  it('test 2', function (done) {
+       
     assert.equal(person.firstName + " " + person.lastName,"Avita Lisbaron","The firstname is not correct");
-
+    setTimeout(done,500); 
 });
 });
